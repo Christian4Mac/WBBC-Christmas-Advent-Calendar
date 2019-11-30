05c433b7aeb7952ced99d9a8a3074578b27f9db3
Christian McClanahan <2chay713@gmail.com>
2019-11-30 18:09:09 -0500

    Added homepage, instructions, and conditional redirect to WBBC site. Need to redo imagemap hotspots.

---
+++ main.py
@@ -0,0 +1,25 @@
+import datetime
+from flask import Flask, request, render_template, url_for, redirect
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/instructions")
+def instructions():
+    return render_template('instructions.html')
+
+@app.route("/go_to_day")
+def go_to_day():
+    day = int(request.args.get('day'))
+    dt = datetime.datetime.today()
+    if day <= dt.day or dt.month < 4:
+        return redirect(f'http://www.westbradenton.org/family-advent/posts/december-{day}')
+    else:
+        return render_template('too_early.html')
+
+# Run flask
+if __name__ == '__main__':
+    app.run(debug=True)
