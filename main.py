bcd6f8f9677b6ae2ac35a6d3f14b716f5866bf1c
Christian McClanahan <2chay713@gmail.com>
2019-11-30 19:51:22 -0500

    Changed 'Oops too early' to a nicer 'Coming soon...'

--- main.py
+++ main.py
@@ -23,5 +23,5 @@ def go_to_day():
 
 # Run flask
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=80)
